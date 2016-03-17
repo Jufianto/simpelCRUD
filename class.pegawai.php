36a20ebfa70c5579b007dc80de6393bd0d2217e2
jufianto <jufiantohendri@gmail.com>
2016-03-17 21:35:06 +0700

    added file class pegawai

--- class.pegawai.php
+++ class.pegawai.php
@@ -26,6 +26,19 @@ class pegawai
 		
 	}
 
+	public function generateId($date)
+	{
+		$spldate = explode("-", $date); // explode date by -
+        $years = substr($spldate[2], 2); 
+        /* get years on date by last two number ex 2015 will get 15 */
+        $month = $spldate[1]; //get month
+        $date = $spldate[0]; //get date
+        $rand = rand();
+        $rand = substr($rand, 0 , 3);
+        $rand1 = rand(100, 999);
+        return $id = $years.$month.$date.$rand1.$rand;
+	}
+
 
 
 
