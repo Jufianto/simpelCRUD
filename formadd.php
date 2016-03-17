60fde4d32901eeb11a516365c652b2b1aa8adcc7
jufianto <jufiantohendri@gmail.com>
2016-03-17 14:52:48 +0700

    changes back

--- formadd.php
+++ formadd.php
@@ -49,7 +49,7 @@
 		<div class="panel panel-default panel-primary">
 			<div class="panel-heading"><h5>Nice Word</h5></div>
 			<div class="panel-body">
-				Hidup itu seperti naik sepeda, agar tetap seimbang, kau harus terus bergerak
+				Hidup itu seperti naik sepeda, agar tetap seimbang, kau harus terus bergerak 
 			</div>
 			<div class="panel-footer"><i>Albert Einstein</i></div>
 		</div>
